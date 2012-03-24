b8979fcc3e8d5f9345f6d150fc162b71634a45f5
Ben Sheldon <bensheldon@gmail.com>
2012-03-24 11:18:46 -0700

    Refactored Square and Map classes. Moved AdjacentSquares functions into Map and generally tried to make Square more self contained.

--- models/map.js
+++ models/map.js
@@ -1,27 +1,24 @@
-var Square = require('./square.js').Square;
+var Square = require('./square.js').Square
+	, _ = require('underscore');
 
 var Map = function(mapsize, emitter) {
   this.age = 0;
   this.emitter = emitter;
   this.mapsize = mapsize;
-  this.population = 0;
-  this.countZones = {
-    residential: 0,
-    commercial: 0,
-    industrial: 0
-  };
+  this.residents = 0;
+  this.jobs = 0;
   this.demand = {
     residential: 1,
     commercial: 1,
     industrial: 1
-  }
-  
+  };
+      
   // Setup our squares
   this.squares = [mapsize];
   for(var x=0; x < this.mapsize; x++) {
     var column = [this.mapsize];
     for(var y=0; y < this.mapsize; y++) {
-      column[y] = new Square(this, [x,y], null);
+      column[y] = new Square(this, [x,y]);
     }
     this.squares[x] = column;
   }
@@ -39,11 +36,68 @@ Map.prototype.scanMap = function(callback) {
   }
 };
 
+/**
+ * Returns a list of positions of squares adjacent to the input position
+ */
+Map.prototype.adjacentSquares = function(position, distance) {
+  var squarePositions = [];
+  
+  if (distance === 0) {
+		if(position[1] - 1 > 0) { // North
+			squarePositions.push([position[0],position[1]-1]);
+		}
+		if (position[0] + 1 < this.mapsize) { // East
+			squarePositions.push([position[0]+1, position[1]]);
+		}
+		if (position[1] + 1 < this.mapsize) { // South
+			squarePositions.push([position[0], position[1]+1]);
+		}
+		if (position[0] - 1 > 0) { // West
+			squarePositions.push([position[0] -1 , position[1]]);
+		}
+	}
+	else { // distance > 0  
+	  // Starting in the North-West corner, scan adjacent squares
+	  for (var y = position[1] - distance; y <= position[1] + distance; y++) {
+	    if ( (y >= 0) && (y < this.mapsize)) { // not beyond border of map
+	      for (var x = position[0] - distance; x <= position[0] + distance; x++) {
+	        if ( (x >= 0) && (x < this.mapsize) ) { // not beyond border of map
+	          if ( (x !== position[0]) || (y !== position[1]) ) { // not the square itself
+	            squarePositions.push([x,y]);
+	          }
+	        }
+	      }
+	    }
+	  }
+	}
+  return squarePositions;
+};
+
+Map.prototype.adjacentSquaresCount = function (position, distance) {
+  var count = {};
+  
+  // get the adjacent squares and iterate
+  var adjacentSquares = this.adjacentSquares(position, distance);
+  for (var i = 0; i < adjacentSquares.length; i++) {
+    var position = adjacentSquares[i];
+    var square = this.squares[position[0]][position[1]];
+    if (count[square.zone] === undefined) {
+			count[square.zone] = [square.position];
+		}
+		else {
+			count[square.zone].push(square.position);
+		}
+  }
+  return count;
+};
+
 Map.prototype.updateUiMap = function() {
   var data = {
     map: {
       age: this.age,
-      population: this.population
+      residents: this.residents,
+      jobs: this.jobs,
+      demand: this.demand
     }
   }
   this.emitter.emit('updateMap', data);
@@ -54,7 +108,9 @@ Map.prototype.updateUiSquare = function(square) {
     square: {
       position: square.position,
       zone: square.zone,
-      density: square.density
+      residents: square.residents,
+      jobs: square.jobs,
+      pollution: square.pollution
     }
   }
   this.emitter.emit('updateSquare', data);
--- models/square.js
+++ models/square.js
@@ -30,27 +30,30 @@ Square.prototype.calcResidents = function() {
 
 Square.prototype.calcResidents = function(globalDemand) {
   var growResidents = 0
-  // if population is zero
-  if (this.residents == 0) {
-  	if (globalDemand.residential > 0) {
-       growResidents = 1; // default growth;
-       this.growthRate = 100;
-       this.residents += growResidents;
-  	}
-    else {
-      this.growthRate = 0;
+  
+  if ( (globalDemand.residential > 0) 
+    && (this.transit == true) ) {
+      
+    // if population is zero
+    if (this.residents == 0) {
+      growResidents = 1; // default growth;
+      this.growthRate = 100;
+      this.residents += growResidents;
     }
-  }
-  else {
-	  if ( (globalDemand.residential > 0) 
-      && (this.transit == true) ) { 
-
+    else { // (this.residents > 0)
       growResidents = this.residents * (globalDemand.residential / 100);
       growResidents = Math.ceil( growResidents * ((5 - this.pollution) / 5) );
       this.growthRate = Math.round( (growResidents / this.residents) * 100 );
       this.residents += growResidents;
       this.residents = Math.min(this.residents, 1000);
-    } else {
+    }
+  } else {
+    if (this.residents == 0) {
+      this.growthRate = 0;
+      this.residents = 0;
+    }
+    else { // (this.residents > 0)
+      
       if (this.transit == true) {
         // globalDemand is now negative
         growResidents = 0.3 * this.residents * (globalDemand.residential / 100);
@@ -60,7 +63,7 @@ Square.prototype.calcResidents = function(globalDemand) {
         this.residents += growResidents;
         this.residents = Math.max(this.residents, 0);
       }
-      else { 
+      else { // (this.transit == false)
         // globalDemand but no transit connection 
         growResidents = -0.10 * this.residents;
         this.growthRate = Math.round( (growResidents / this.residents) * 100 );
@@ -70,47 +73,5 @@ Square.prototype.calcResidents = function(globalDemand) {
     }
   }
 }
-
-
-
-
-
-/**
- * Returns a list of positions of squares adjacent to the input position
- */
-Square.prototype.adjacentSquares = function(distance) {
-  var squarePositions = [];
-  
-  // Starting in the North-West corner, scan adjacent squares
-  for (var y = this.position[1] - distance; y <= this.position[1] + distance; y++) {
-    if ( (y >= 0) && (y < this._map.mapsize)) { // not beyond border of map
-      for (var x = this.position[0] - distance; x <= this.position[0] + distance; x++) {
-        if ( (x >= 0) && (x < this._map.mapsize) ) { // not beyond border of map
-          if ( (x !== this.position[0]) || (y !== this.position[1]) ) { // not the square itself
-            squarePositions.push([x,y]);
-          }
-        }
-      }
-    }
-  }
-  return squarePositions;
-};
-
-Square.prototype.adjacentSquaresCount = function (distance) {
-  var count = {
-    residential: 0,
-    commercial: 0,
-    industrial: 0
-  };
-  
-  // get the adjacent squares and iterate
-  var squarePositions = this.adjacentSquares(distance);
-  for (var i = 0; i < squarePositions.length; i++) {
-    var position = squarePositions[i];
-    var square = this._map.squares[position[0]][position[1]];
-    count[square.zone]++;
-  }
-  return count;
-};
 
 exports.Square = Square;
