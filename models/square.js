a3eb0447173c30281225b51d4031b276a6f51660
Ben Sheldon <bensheldon@gmail.com>
2012-03-30 12:30:45 -0700

    Changed made up globalDemand to the map.demand and cleaned up rezone()

--- models/square.js
+++ models/square.js
@@ -1,20 +1,23 @@
 var Square = function(map, position) {
-  this.zone = null; 		  // type of zone
-  this.residents = 0; 		// how many people live here (0 <> 1000)
-  this.jobs = 0; 			    // how many people can be employed here (0 <> 600)
-  this.growthRate = 0; 		// how quickly are residents/jobs increasing (-200 <> 200)
-  this.landValue = 0; 		// what is the perceived value of this square (0 <> 500,000)
-  this.transit = false; 	// can other zones be reached (true | false)
-  this.pollution = 0; 		// level of pollution (0 <> 5)
-  this.polluter = false;	// the zone produces pollution (true | false)
-  this.density = 0;			  // how jammed together is the zone (0 <> 5)
   this.position = [position[0], position[1]];
-  this._map = map;			  // the map the square is a part of
+  this.zone = null;         // type of zone
+  this.residents = 0;       // how many people live here (0 <> 1000)
+  this.jobs = 0;            // how many people can be employed here (0 <> 600)
+  this.growthRate = 0;      // how quickly are residents/jobs increasing (-200 <> 200)
+  this.landValue = 0;       // what is the perceived value of this square (0 <> 500,000)
+  this.transit = true;     // can other zones be reached (true | false)
+  this.pollution = 0;       // level of pollution (0 <> 5)
+  this.polluter = false;    // the zone produces pollution (true | false)
+  this.density = 0;         // how jammed together is the zone (0 <> 5)
+  this._map = map;          // the map the square is a part of
 };
 
 Square.prototype.rezone = function(zone) {
-  this.zone = zone;
-  this.population = 0;
+  this.zone      = zone;
+  this.residents = 0;
+  this.jobs      = 0;
+  this.density   = 0;
+  this.transit   = false;
   this._map.updateUiSquare(this);
 };
 
@@ -28,10 +31,10 @@ Square.prototype.calcResidents = function() {
   }
 }
 
-Square.prototype.calcResidents = function(globalDemand) {
+Square.prototype.calcResidents = function() {
   var growResidents = 0
   
-  if ( (globalDemand.residential > 0) 
+  if ( (this._map.demand.residential > 0) 
     && (this.transit == true) ) {
       
     // if population is zero
@@ -41,7 +44,7 @@ Square.prototype.calcResidents = function(globalDemand) {
       this.residents += growResidents;
     }
     else { // (this.residents > 0)
-      growResidents = this.residents * (globalDemand.residential / 100);
+      growResidents = this.residents * (this._map.demand.residential / 100);
       growResidents = Math.ceil( growResidents * ((5 - this.pollution) / 5) );
       this.growthRate = Math.round( (growResidents / this.residents) * 100 );
       this.residents += growResidents;
@@ -56,7 +59,7 @@ Square.prototype.calcResidents = function(globalDemand) {
       
       if (this.transit == true) {
         // globalDemand is now negative
-        growResidents = 0.3 * this.residents * (globalDemand.residential / 100);
+        growResidents = 0.3 * this.residents * (this._map.demand.residential / 100);
         growResidents = Math.ceil( growResidents * ( 1 + this.pollution / 10 ) );
           // pollution should accelerate the negative growth, here it accelerates to twice
         this.growthRate = Math.round( (growResidents / this.residents) * 100 );
